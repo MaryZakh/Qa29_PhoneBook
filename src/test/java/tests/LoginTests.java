db3ba016487626f90928e949c6e0134b5b5aac0d
Maria Z <z0559882272@gmail.com>
2025-05-04 21:04:51 +0300

    added login success test without assert

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -5,7 +5,10 @@ import org.testng.annotations.Test;
 public class LoginTests extends TestBase{
 
     @Test
-    public void test(){
+    public void loginSuccess(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("mara@gmail.com","Mmar123456$");
+        app.getHelperUser().submitLogin();
 
     }
 
