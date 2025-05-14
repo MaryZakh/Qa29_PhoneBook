6932055ae439ed29a91aee2ac24c95d8236defb4
Maria Z <z0559882272@gmail.com>
2025-05-14 18:58:07 +0300

    added RegistrationTests

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,7 +1,12 @@
 package manager;
 
+import models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HelperUser extends HelperBase {
 
@@ -24,17 +29,23 @@ public class HelperUser extends HelperBase {
 //        emailInput.click();
 //        emailInput.clear();
 //        emailInput.sendKeys(email);
-        type(By.name("email"),email);
+        type(By.name("email"), email);
 
 //        WebElement passwordInput = wd.findElement(By.xpath("//input[last()]"));
 //
 //        passwordInput.click();
 //        passwordInput.clear();
 //        passwordInput.sendKeys(password);
-        type(By.xpath("//input[last()]"),password);
+        type(By.xpath("//input[last()]"), password);
     }
 
-    public void submitLogin(){
+    public void fillLoginRegistrationForm(User user) {
+
+        type(By.name("email"), user.getEmail());
+        type(By.xpath("//input[last()]"), user.getPassword());
+    }
+
+    public void submitLogin() {
         click(By.xpath("//button[text()='Login']"));
     }
 
@@ -44,5 +55,17 @@ public class HelperUser extends HelperBase {
 
     public void logout() {
         click(By.xpath("//button[text()='Sign Out']"));
+    }
+
+    public void submitRegistration() {
+        click(By.xpath("//button[text()='Registration']"));
+    }
+
+    public boolean isNoContactsHereDisplayed() {
+        WebDriverWait wait = new WebDriverWait(wd, Duration.ofSeconds(5));
+        boolean res = wait.until(ExpectedConditions.textToBePresentInElement
+                (wd.findElement(By.cssSelector(".contact-page_message__2qafk>h1")),
+                        "No Contacts here!"));
+        return res;
     }
 }
