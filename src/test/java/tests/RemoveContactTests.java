58cfb499e1455d58cd1f64b02e5766ba279558c9
Maria Z <z0559882272@gmail.com>
2025-05-21 21:01:48 +0300

    added homeWork delete contacts

--- src/test/java/tests/RemoveContactTests.java
+++ src/test/java/tests/RemoveContactTests.java
@@ -1,4 +1,29 @@
 package tests;
 
+import models.User;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class RemoveContactTests extends TestBase{
+
+    @BeforeMethod
+    public void preCondition(){
+        if(!app.getHelperUser().isLogged()){
+            app.getHelperUser().login(new User().withEmail("margo@gmail.com").withPassword("Mmar123456$"));
+        }
+
+       // app.getHelperContact().provideContacts();
+    }
+
+    @Test
+    public void removeFirstContact(){
+        //Asset size list less by one
+    }
+
+    @Test
+    public void removeAllContacts(){
+        //"No Contacts Here"
+    }
+
+
 }
